f5cec65265e248dc1be1f0759825e1d0bd4f5cd9
Fardin-Mohammed <39059@ma-web.nl>
2024-12-08 14:15:10 +0100

    changes

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program(); // Maak een nieuw object van de klasse Program.
+            program.Run(); // Roep de Run-methode aan om het programma te starten.
+        }
+
+        internal void Run() 
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma");
         }
     }
 }
