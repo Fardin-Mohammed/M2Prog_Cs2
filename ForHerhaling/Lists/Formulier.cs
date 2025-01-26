2cec0a4d1b9057e05f54c5690fc36ee0e354e974
Fardin-Mohammed <39059@ma-web.nl>
2025-01-26 13:48:57 +0100

    Lists project afgerond met arrays en lists

---
+++ ForHerhaling/Lists/Formulier.cs
@@ -0,0 +1,11 @@
+﻿public class Formulier
+{
+    public string Naam { get; set; }
+    public string Review { get; set; }
+
+    public Formulier(string naam, string review)
+    {
+        Naam = naam;
+        Review = review;
+    }
+}
---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Run();
+    }
+
+    static void Run()
+    {
+        string[] characters = new string[]
+        {
+            "Pac-Man", "Samus", "Crash Bandicoot", "Spyro the Dragon",
+            "Donkey Kong", "Mario", "Luigi", "Astro",
+            "Solid Snake", "Master Chief", "Kratos", "Sonic the Hedgehog", "Link"
+        };
+
+        List<string> characterList = new List<string>();
+        characterList.Add("Yoshi"); 
+
+        foreach (string character in characters)
+        {
+            characterList.Add(character);
+        }
+
+        Console.WriteLine("Characters (for-loop):");
+        for (int i = 0; i < characterList.Count; i++) 
+        {
+            Console.WriteLine($"- {characterList[i]}");
+        }
+
+        Console.WriteLine("\nCharacters (foreach-loop):");
+        foreach (string character in characterList)
+        {
+            Console.WriteLine($"- {character}");
+        }
+    }
+}
